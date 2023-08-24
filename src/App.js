65ed5a3c05bdfa30975bc50b8b05c75d15c9842a
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-24 21:20:44 +0200

    d

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import UseRefExample from "./components/UseRefExample";
+import UseRefExample2 from "./components/UseRefExample2";
 
 function App() {
   return (
     <div className="App mt-5">
-      <UseRefExample />
+      {/* <UseRefExample /> */}
+      <UseRefExample2 />
     </div>
   );
 }
---
+++ src/components/UseRefExample2.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef, useState } from "react";
+
+function UseRefExample2() {
+  const [name, setName] = useState("");
+  const renders = useRef(1);
+  const prevName = useRef("");
+
+  useEffect(() => {
+    renders.current = renders.current + 1;
+    prevName.current = name;
+  }, [name]);
+  return (
+    <div>
+      <h1> Renders: {renders.current}</h1>
+      <h2>Prev Name State: {prevName.current}</h2>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        className="form-control"
+      />
+    </div>
+  );
+}
+
+export default UseRefExample2;
