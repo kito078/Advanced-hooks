2c795b67791e770f2aa67a822d9dab4ce1e8ba55
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-25 08:43:15 +0200

    s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import UseRefExample from "./components/UseRefExample";
 import UseRefExample2 from "./components/UseRefExample2";
+import UseRefExample3 from "./components/UseRefExample3";
 
 function App() {
   return (
     <div className="App mt-5">
       {/* <UseRefExample /> */}
-      <UseRefExample2 />
+      {/* <UseRefExample2 /> */}
+      <UseRefExample3 />
     </div>
   );
 }
---
+++ src/components/UseRefExample3.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UseRefExample3() {
+  return <div>3</div>;
+}
+
+export default UseRefExample3;
