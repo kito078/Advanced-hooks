29e954d7061c18ffef3af21cc186e1cf8c0ac869
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-25 10:51:44 +0200

    w

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Todo() {
+  const [loding, setLoading] = useState(true);
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    fetch("")
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          setTodo(data);
+          setLoading(false);
+        }, 3000);
+      });
+  }, []);
+
   return <div>tod</div>;
 }
 
